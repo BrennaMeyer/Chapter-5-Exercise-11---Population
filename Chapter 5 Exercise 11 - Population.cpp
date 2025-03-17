f18a12afbeea6068a0ed5384425957687c8d10ac
Brenna Meyer <brennameyer9@gmail.com>
2025-03-17 18:21:21 -0400

    Add project files.

---
+++ Chapter 5 Exercise 11 - Population.cpp
@@ -0,0 +1,26 @@
+// Chapter 5 Exercise 11 - Population.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Programmer:Brenna Meyer
+//Date:03/17/2025
+//Requirements:Write a program that will predict the size of a population of organisms.
+//The program should ask the user for the starting number of organisms,their average daily population increase(as a percentage),
+//and the number of days they will multiply.A loop should display the size of the population for each day.
+// //Input Validation : Do not accept a number less than 2 for the starting size of the population.
+//Do not accept a negative number for average daily population increase. Do not accept a number less than 1 for the number of days they will multiply.
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
