22ae331fa2dc1d8185be85b6559a0a64b537d176
Brenna Meyer <brennameyer9@gmail.com>
2025-03-19 11:24:38 -0400

    finished

--- Chapter 5 Exercise 11 - Population.cpp
+++ Chapter 5 Exercise 11 - Population.cpp
@@ -8,19 +8,52 @@
 //Do not accept a negative number for average daily population increase. Do not accept a number less than 1 for the number of days they will multiply.
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float starting_num_organisms,
+        daily_pop_increase,
+        size_of_daily_pop;
+
+    int num_days_multiply;
+    //number organisms
+    cout << "Please enter the starting number of organisms:";
+
+    while (!(cin >> starting_num_organisms) || starting_num_organisms < 2)
+    {
+        cout << "Starting number must be 2 or more." << endl;
+        cout << "Enter the starting number of organisms:";
+    
+    }
+    //Daily population
+    cout << "Please enter the average daily population increase by percentage:";
+
+    while (!(cin >> daily_pop_increase) || daily_pop_increase < 0)
+    {
+        cout << "Average daily population increase must be greater than 0. \n"
+            << "Enter average daily population increase by percentage:";
+          }
+
+    daily_pop_increase *= 0.01;
+    //Number days Multiply 
+    cout << "Enter the number of days they will multiply:";
+
+    while (!(cin >> num_days_multiply) || num_days_multiply<1)
+    {
+        cout << "The number of days must NOT be less than 1.\n"
+            << "Enter the number of days they will multiply:";
+         }
+
+    for (int i = 0; i < num_days_multiply; i++)
+    {
+        cout << "Population size for day" << (i + 1);
+        cout << ":" << starting_num_organisms << endl;
+
+        starting_num_organisms += (starting_num_organisms * daily_pop_increase);
+    }
+
+    return 0;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
